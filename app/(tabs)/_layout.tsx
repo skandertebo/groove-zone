7613ccb9f189e9c7c16e0043b72d9eb970429d95
Skander Tebourbi <alexandertebourb@gmail.Com>
2024-08-21 12:00:00 +0100

    initial commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,99 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from "expo-router";
+import React, { createContext, useContext, useEffect, useState } from "react";
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import S3 from "aws-sdk/clients/s3";
+
+const musicContext = createContext({
+  music: [] as Array<{
+    url: string;
+    name: string;
+  }>,
+  loading: true,
+});
+
+export const useMusic = () => {
+  return useContext(musicContext);
+};
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
+  const [music, setMusic] = useState<
+    Array<{
+      url: string;
+      name: string;
+    }>
+  >([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const s3 = new S3({
+      accessKeyId: process.env.EXPO_PUBLIC_AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.EXPO_PUBLIC_AWS_SECRET_ACCESS_KEY,
+      region: "us-east-1",
+    });
+
+    s3.listObjectsV2(
+      {
+        Bucket: "skanderfiles",
+        Prefix: "music/",
+      },
+      (err, data) => {
+        if (err) {
+          console.log("Error", err);
+          setLoading(false);
+        } else {
+          const music = data.Contents?.filter((o) => o.Key !== "music/").map(
+            (f) => ({
+              url: `https://skanderfiles.s3.amazonaws.com/${f.Key}`,
+              name:
+                f.Key?.split("/").pop()?.split(".").slice(0, -1).join(".") ??
+                "",
+            })
+          );
+          setMusic(music ?? []);
+          setLoading(false);
+        }
+      }
+    );
+  });
+
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
+    <musicContext.Provider value={{ music, loading }}>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+          headerShown: false,
         }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Home",
+            tabBarIcon: ({ color, focused }) => (
+              <TabBarIcon
+                name={focused ? "home" : "home-outline"}
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="explore"
+          options={{
+            title: "Explore",
+            tabBarIcon: ({ color, focused }) => (
+              <TabBarIcon
+                name={focused ? "code-slash" : "code-slash-outline"}
+                color={color}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+    </musicContext.Provider>
   );
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,259 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import {
+  Animated,
+  Button,
+  Pressable,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import {
+  Audio,
+  AVPlaybackStatus,
+  InterruptionModeAndroid,
+  InterruptionModeIOS,
+} from "expo-av";
+import { useEffect, useRef, useState } from "react";
+import { useMusic } from "./_layout";
+
+type FullSong = Audio.SoundObject & { uri: string; name: string };
 
 export default function HomeScreen() {
+  const { music, loading } = useMusic();
+  const [selectedSong, _setSelectedSong] = useState<FullSong | null>(null);
+  const initialPlayRef = useRef(true);
+  const [isSongPlaying, setIsSongPlaying] = useState(false);
+
+  function toggleSong(selectedSong: Audio.Sound) {
+    let promise;
+    if (selectedSong) {
+      if (isSongPlaying) {
+        promise = selectedSong.pauseAsync();
+      } else {
+        promise = selectedSong.playAsync();
+      }
+      setIsSongPlaying((prev) => !prev);
+      return promise;
+    }
+    return Promise.resolve();
+  }
+
+  async function setSelectedSong(song: { uri: string; name: string }) {
+    if (selectedSong) {
+      await selectedSong.sound.unloadAsync();
+    }
+
+    await Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+      staysActiveInBackground: true,
+      interruptionModeIOS: InterruptionModeIOS.DuckOthers,
+      playsInSilentModeIOS: true,
+      shouldDuckAndroid: true,
+      interruptionModeAndroid: InterruptionModeAndroid.DuckOthers,
+      playThroughEarpieceAndroid: false,
+    });
+    const songObject = await Audio.Sound.createAsync({ uri: song.uri });
+    setIsSongPlaying(true);
+    await songObject.sound.playAsync();
+    _setSelectedSong({
+      ...songObject,
+      uri: song.uri,
+      name: song.name,
+    });
+  }
+
+  useEffect(() => {
+    if (selectedSong) return;
+    if (music.length && !selectedSong && initialPlayRef.current) {
+      initialPlayRef.current = false;
+      setSelectedSong({
+        uri: music[0].url,
+        name: music[0].name,
+      });
+    }
+  }, [music]);
+
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
       headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+        <MusicPlayer
+          song={selectedSong}
+          loading={loading || !selectedSong}
+          toggleSong={toggleSong}
+          isSongPlaying={isSongPlaying}
         />
-      }>
+      }
+    >
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Welcome!</ThemedText>
         <HelloWave />
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+      <Animated.ScrollView style={styles.stepContainer}>
+        {music.map((song) => (
+          <View
+            style={{
+              marginVertical: 8,
+            }}
+            key={song.url}
+          >
+            <Button
+              title={song.name}
+              onPress={() =>
+                setSelectedSong({
+                  uri: song.url,
+                  name: song.name,
+                })
+              }
+            />
+          </View>
+        ))}
+      </Animated.ScrollView>
     </ParallaxScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
   },
   stepContainer: {
     gap: 8,
+    display: "flex",
+    flexDirection: "column",
     marginBottom: 8,
+    overflow: "scroll",
+    height: 300,
   },
   reactLogo: {
     height: 178,
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
+  },
+  musicPlayer: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: 250,
+    overflow: "hidden",
   },
 });
+
+export function MusicPlayer({
+  song,
+  loading,
+  toggleSong,
+  isSongPlaying,
+}: {
+  song: FullSong | null;
+  loading: boolean;
+  toggleSong: (song: Audio.Sound) => Promise<any>;
+  isSongPlaying: boolean;
+}) {
+  const [info, setInfo] = useState<AVPlaybackStatus | null>(null);
+  const scheme = useColorScheme();
+  useEffect(() => {
+    if (!song) return;
+    song.sound.getStatusAsync().then(setInfo);
+    const interval = setInterval(() => {
+      song.sound.getStatusAsync().then(setInfo);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [song]);
+
+  return (
+    <ThemedView style={styles.musicPlayer}>
+      {loading ? (
+        <ThemedText>Loading...</ThemedText>
+      ) : (
+        <>
+          <ThemedText>{song?.name}</ThemedText>
+          {info && (
+            <ThemedText>
+              {info.isLoaded && info.durationMillis ? (
+                <View
+                  style={{ display: "flex", flexDirection: "column", gap: 2 }}
+                >
+                  <Text
+                    style={{
+                      textAlign: "right",
+                      fontSize: 12,
+                      color: scheme === "dark" ? "white" : "black",
+                    }}
+                  >
+                    {`${formatDuration(
+                      Math.floor(info.positionMillis / 1000)
+                    )} / ${formatDuration(
+                      Math.floor(info.durationMillis / 1000)
+                    )}`}
+                  </Text>
+                  <SongProgressBar
+                    progress={info.positionMillis / info.durationMillis}
+                  />
+                </View>
+              ) : (
+                "Loading..."
+              )}
+            </ThemedText>
+          )}
+          <View
+            style={{
+              marginTop: 18,
+            }}
+          >
+            <Pressable
+              onPress={() => {
+                if (!song) return;
+                toggleSong(song.sound);
+              }}
+              style={{
+                padding: 8,
+                backgroundColor: "rgba(140,0,0,0.5)",
+                borderRadius: 8,
+                elevation: 8,
+              }}
+            >
+              <ThemedText>{isSongPlaying ? "Pause" : "Play"}</ThemedText>
+            </Pressable>
+          </View>
+        </>
+      )}
+    </ThemedView>
+  );
+}
+
+function SongProgressBar({ progress }: { progress: number }) {
+  return (
+    <ThemedView
+      style={{
+        width: 300,
+        height: 10,
+        backgroundColor: "rgba(133,133,133,0.1)",
+        borderRadius: 5,
+        overflow: "hidden",
+      }}
+    >
+      <ThemedView
+        style={{
+          width: `${progress * 100}%`,
+          height: "100%",
+          backgroundColor: "rgba(80,0,0,0.5)",
+        }}
+      />
+    </ThemedView>
+  );
+}
+
+function formatDuration(duration: number) {
+  const minutes = Math.floor(duration / 60);
+  const seconds = Math.floor(duration % 60);
+  return `${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+}
